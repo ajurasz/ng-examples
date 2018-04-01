5a70dbee291f808dc3530f3ce661d812de0a75bb
ajurasz <arkadiusz.jurasz@gmail.com>
2018-04-01 20:46:28 +0200

    [Recipe Book] Unsubscribe.

--- n5-complete-guide/src/app/shopping-list/shopping-list.component.ts
+++ n5-complete-guide/src/app/shopping-list/shopping-list.component.ts
@@ -1,23 +1,29 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 
 import { ShoppingListService } from './shopping-list.service';
 import { Ingredient } from '../shared/ingredient.model';
+import { Subscription } from 'rxjs/Subscription';
 
 @Component({
   selector: 'app-shopping-list',
   templateUrl: './shopping-list.component.html',
   styleUrls: ['./shopping-list.component.css']
 })
-export class ShoppingListComponent implements OnInit {
+export class ShoppingListComponent implements OnInit, OnDestroy {
+  ingredientsSubscription: Subscription;
   ingredients: Ingredient[];
 
   constructor(private shoppingListService: ShoppingListService) {}
 
   ngOnInit() {
-    this.shoppingListService
+    this.ingredientsSubscription = this.shoppingListService
       .getIngredients()
       .subscribe(
         (ingredients: Ingredient[]) => (this.ingredients = ingredients)
       );
+  }
+
+  ngOnDestroy() {
+    this.ingredientsSubscription.unsubscribe();
   }
 }
