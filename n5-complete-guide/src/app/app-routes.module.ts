e5d1ec08eea6a2005a1fe6fac7b076ac47f86966
ajurasz <arkadiusz.jurasz@gmail.com>
2018-04-02 22:10:09 +0200

    [Recipe Book] Added empty path.

--- n5-complete-guide/src/app/app-routes.module.ts
+++ n5-complete-guide/src/app/app-routes.module.ts
@@ -5,7 +5,8 @@ import { RecipesComponent } from './recipes/recipes.component';
 import { ShoppingListComponent } from './shopping-list/shopping-list.component';
 
 const routes: Routes = [
-  { path: '', component: RecipesComponent },
+  { path: '', redirectTo: '/recipes', pathMatch: 'full' },
+  { path: 'recipes', component: RecipesComponent },
   { path: 'shopping-list', component: ShoppingListComponent }
 ];
 
